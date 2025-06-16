1db7462f53b776e75ce2e17f45f99f63c54e7bd1
Omkar <omkar.waghmare@sticsoftsolutions.com>
2025-06-16 16:51:28 +0530

    Change heading to production

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
             backgroundImage: `url('https://plus.unsplash.com/premium_photo-1673697239981-389164b7b87f?q=80&w=2044&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
         }}
     >
+        <h1>Production level currecy converter</h1>
         <div className="w-full">
             <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                 <form onSubmit={(e)=>{
